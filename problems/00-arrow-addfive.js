dee9f8f8f922f60ac67699d4252e1fb4f887bf3d
Omega Dziko F. <megaoy@yahoo.com>
2024-04-03 20:17:17 -0400

    1st prb slvd

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -15,7 +15,8 @@ let result3 = addFive(37); // returns 42
 
 ***********************************************************************/
 
-// Your code here 
+//implicitly return num plus 5
+let addFive = (num) => num + 5;
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
